be7949eafd88add6ea3cce518287af8c8d60dd45
arefin6 <imranhchy@gmail.com>
2022-05-25 11:25:31 +0600

    JWT implemented

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
@@ -15,6 +16,21 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     await client.connect();
@@ -23,6 +39,7 @@ async function run() {
     const orderCollection = client.db("power_tool_bd").collection("orders");
     const reviewCollection = client.db("power_tool_bd").collection("reviews");
     const paymentCollection = client.db("power_tool_bd").collection("payment");
+    const userCollection = client.db("power_tool_bd").collection("users");
 
     app.get("/tools", async (req, res) => {
       const query = {};
@@ -38,21 +55,21 @@ async function run() {
       res.send(tool);
     });
 
-    app.get("/order", async (req, res) => {
+    app.get("/order", verifyJWT, async (req, res) => {
       const query = {};
       const cursor = orderCollection.find(query);
       const orders = await cursor.toArray();
       res.send(orders);
     });
 
-    app.get("/order/:id", async (req, res) => {
+    app.get("/order/:id", verifyJWT, async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await orderCollection.findOne(query);
       res.send(result);
     });
 
-    app.get("/my-order", async (req, res) => {
+    app.get("/my-order", verifyJWT, async (req, res) => {
       const user = req.query.user;
       const query = { email: user };
       const myOrder = await orderCollection.find(query).toArray();
@@ -99,6 +116,23 @@ async function run() {
       const review = req.body;
       const result = await reviewCollection.insertOne(review);
       res.send(result);
+    });
+
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "3d" }
+      );
+      res.send({ result, token });
     });
 
     app.post("/create-payment-intent", async (req, res) => {
